88323225a2cba9cdb054f82baf7d844f391622ad
tonga54 <grodriguez160597@gmail.com>
2021-02-16 20:35:49 -0300

    driver

--- strategy/scraperContext.py
+++ strategy/scraperContext.py
@@ -11,7 +11,7 @@ class ScraperContext:
     def setDriver(self, driver = "chrome"):
         if driver == "chrome":
             GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-            CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver' 
+            CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
             chrome_options = webdriver.ChromeOptions()
             chrome_options.add_argument('--disable-gpu')
